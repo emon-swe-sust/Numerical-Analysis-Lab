f09a4ea8bc269d916b148ca508349c1398000c3d
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-10-30 03:36:33 +0600

    Create Eular_Method.m

---
+++ Eular_Method.m
@@ -0,0 +1,15 @@
+f = @(x,y) -x*y^2;
+
+x0 = 0;
+xn = 2;
+y = 1;
+h = 0.01;
+
+printf ('x  \t\t y  \n')
+printf ('%f \t %f\t \n',x0,y);
+
+for x = x0 : h : xn-h
+  x = x+h;
+  y = y + f(x,y)*h;
+  printf ('%f \t %f\t \n',x,y);
+endfor
